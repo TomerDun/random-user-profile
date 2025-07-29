8773ceec35c68ad3795b9b532e1a40e4a787d25c
TomerDun <tomer@dunsky.name>
2025-07-29 16:58:04 +0300

    Added function for getting user data for friends from api

--- apiHandler.js
+++ apiHandler.js
@@ -1,9 +1,16 @@
 // Random User API
-async function getUserData() {
-    // Retrieves data from an API and returns a formatted user
+export async function getUserData() {    
     const url = 'https://randomuser.me/api/'; // Get 1 user
     const res = await fetch(url);
     if (!res.ok) throw Error (`Error retrieving random user from API (Code ${res.statusText})`);
     let data = await res.json()
     return data.results[0];
+}
+
+export async function getFriendsData(amount=6) {
+    const url = 'https://randomuser.me/api/?results=' + amount; // Get 1 user
+    const res = await fetch(url);
+    if (!res.ok) throw Error (`Error retrieving random user from API (Code ${res.statusText})`);
+    let data = await res.json()
+    return data.results;
 }
