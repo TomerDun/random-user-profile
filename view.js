f900602583688233dfbf9b398890d2a798fc6cea
TomerDun <tomer@dunsky.name>
2025-07-30 14:44:57 +0300

    Added some styling

--- view.js
+++ view.js
@@ -1,5 +1,12 @@
 export default function render(user) {
     // User info
     document.querySelector('#user-img').setAttribute('src', user.image);
+    document.querySelector('#user-name').innerHTML = user.name;
+    document.querySelector('#user-location').innerHTML = `${user.city}, ${user.country}`
+
+    document.querySelector('#quote-content').innerHTML = user.quote;
+    document.querySelector('#summary-content').innerHTML = user.summary;
+
+
 
 }
