b5a9162d054a965860074189ea697d3ef9e80b48
TomerDun <tomer@dunsky.name>
2025-07-30 09:24:29 +0300

    Added functions for fetching quote and summary

--- apiHandler.js
+++ apiHandler.js
@@ -14,3 +14,23 @@ export async function getFriendsData(amount=6) {
     let data = await res.json()
     return data.results;
 }
+
+// Random Quote
+export async function getQuote() {
+    const url = 'https://api.kanye.rest';
+    const res = await fetch(url);
+    if (!res.ok) throw Error (`Error retrieving random Quote (Code ${res.statusText})`);
+    let data = await res.json()
+    return data.quote;
+}
+
+// Meat
+export async function getSummary() {
+    const url = 'https://baconipsum.com/api/?type=meat';
+    const res = await fetch(url);
+    if (!res.ok) throw Error (`Error retrieving Summary (Code ${res.statusText})`);
+    let data = await res.json()
+    return data[0];
+}
+
+getSummary().then(console.log)
--- controller.js
+++ controller.js
@@ -1,11 +1,14 @@
 import User from "./userModel.js";
-import { getUserData } from "./apiHandler";
+import { getUserData } from "./apiHandler.js";
 
 const user = new User();
 
 async function createUser() {
-    const userData = getUserData();
+    const userData = await getUserData();
     user.name = `${userData.name.first} ${userData.name.last}`
     user.country = userData.location.country;
     user.city = userData.location.city;
+    
+    console.log(user);
+    
 }
