70f03b7c22d05220261eb48045a880582b44df10
TomerDun <tomer@dunsky.name>
2025-07-30 14:47:47 +0300

    Added friends list to view

--- view.js
+++ view.js
@@ -7,6 +7,14 @@ export default function render(user) {
     document.querySelector('#quote-content').innerHTML = user.quote;
     document.querySelector('#summary-content').innerHTML = user.summary;
 
+    generateFriendsList(user.friends)
+}
 
-
+function generateFriendsList(friends) {
+    const list = document.querySelector('#friends-list');
+    friends.forEach(friend => {
+        const li = document.createElement('li');
+        li.innerHTML = friend;
+        list.appendChild(li);
+    })
 }
